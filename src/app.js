999f453da9e937a7a60cdb02ad8f3042f9349652
justin-dupre <42753529+justin-dupre@users.noreply.github.com>
2020-11-14 19:16:54 -0600

    Add users/ping endpoint (#1)
    * Add users/ping endpoint
    
    * Update src/app.js
    
    Co-authored-by: James Bonasera <jabonasera@gmail.com>
    
    Co-authored-by: James Bonasera <jabonasera@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
 import logger from 'morgan';
@@ -8,24 +8,26 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
-import usersRouter from "./routes/users";
+import usersRouter from './routes/users';
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Connect to Mongo
 mongoose.connect(`${process.env.MONGO_URI}/${process.env.MONGO_DB_NAME}`, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
-db.once('open', () => console.log(`MongoDB:${process.env.MONGO_DB_NAME} Connected...`));
+db.once('open', () =>
+  console.log(`MongoDB:${process.env.MONGO_DB_NAME} Connected...`)
+);
 
 // API Routes
 app.use('/api', indexRouter);
@@ -33,9 +35,9 @@ app.use('/api/users', usersRouter);
 
 // Hello App
 app.use('/', (req, res) => {
-    res.json({message: 'Hello DnDnD'});
+  res.json({ message: 'Hello Template' });
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5000);
 
 export default app;
